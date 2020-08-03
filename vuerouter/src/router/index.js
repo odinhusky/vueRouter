79f599e84be92f7f31716feef626561c9111b253
odinhusky <odinhusky0923@gmail.com>
2020-08-03 21:23:18 +0800

    20200803-2 update

--- vuerouter/src/router/index.js
+++ vuerouter/src/router/index.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 
 import Home from '@/components/HelloWorld'
+import Page from '@/components/pages/page'
+
+
 
 Vue.use(VueRouter)
 
@@ -11,6 +14,11 @@ export default new VueRouter({
             name: '首頁', // 對應的名稱
             path: '/', // 對應的虛擬路徑
             component: Home // 對應的component
+        },
+        {
+          name: '分頁', // 對應的名稱
+          path: '/page', // 對應的虛擬路徑
+          component: Page // 對應的component
         }
     ]
 })
